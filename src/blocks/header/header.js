49162ef5e67d8803a4b6253240be30f7573a6967
Tony at Home <extracat@users.noreply.github.com>
2019-10-03 23:01:55 +0300

    Update

---
+++ src/blocks/header/header.js
@@ -0,0 +1,18 @@
+
+(function (d, s) {
+
+    var js, fjs = d.getElementsByTagName(s)[0];
+    js = d.createElement(s);
+    js.src = "https://widgets.mos.ru/cnews/citywidgets.js";
+    fjs.parentNode.insertBefore(js, fjs);
+
+    var i = setInterval(function () {
+        if (window.city_widget && document.readyState === "complete") {
+            city_widget('650x200', '.widget3');
+            clearInterval(i);
+        }
+    }, 50);
+    setTimeout(function () {
+        clearInterval(i)
+    }, 5000);
+}(document, 'script'));
